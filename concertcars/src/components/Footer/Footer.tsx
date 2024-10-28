208e15b0b2435116e4f245eca40522c696474702
GuiRomanholi <guilhermeroma343@gmail.com>
2024-10-28 12:45:27 -0300

    Criando Footer e estilizando

--- concertcars/src/components/Footer/Footer.tsx
+++ concertcars/src/components/Footer/Footer.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import Image from "next/image";
+import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+            <div className="footerContainer">
+                <ul className="footerMenu">
+                    <li className="logoFooter">
+                        <Link href={"/"}>
+                            {<Image src="/img/logo_ccs_branco.png" alt="Logo_Concert_Branco" width={200} height={200}/>}
+                        </Link>
+                    </li>
+                    <p className="barraSeparacao">|</p>
+                    <li>
+                        <a href="https://github.com/GuiRomanholi/Challenge-Front-Sprint4" target="_blank" rel="noopener noreferrer">
+                            {<Image src="/img/logo_github_branco.png" alt="Github_Branco" width={200} height={200}/>}
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/concertcars_of" target="_blank" rel="noopener noreferrer">
+                            {<Image src="/img/logo_instagram_branco.png" alt="intstagram_logo" width={200} height={200}/>}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <p>&copy; 2024 Consert Cars. Todos os direitos reservados.</p>
+        </footer>
   )
 }
