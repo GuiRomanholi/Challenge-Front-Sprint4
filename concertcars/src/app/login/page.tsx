eeb2f690538b423a393cce1a0e9351b0bbfa57d5
GuiRomanholi <guilhermeroma343@gmail.com>
2024-10-29 16:08:55 -0300

    Criando e estilizando o login

--- concertcars/src/app/login/page.tsx
+++ concertcars/src/app/login/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+
+import Link from 'next/link';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='lo-loginContainer'>
+          <form id="form-login" className='lo-loginForm'>
+            <h2>Login</h2>
+            <input type="text" name="txtLogin" id="idLogin" className='lo-inputField' placeholder="Usuário" required/>
+            <input type="password" name="txtSenha" id="idSenha" className='lo-inputField' placeholder="Senha" required/>
+            <button type="submit" className='lo-submitButton'>Entrar</button>
+            <Link className='lo-createAccountLink' href={"/cadastro"}>Criar uma conta</Link>
+          </form>
+    </div>
   )
 }
