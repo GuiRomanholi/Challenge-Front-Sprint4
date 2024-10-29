776482ab9fe0a32071e4413d1e40a1a2ee6e3380
GuiRomanholi <guilhermeroma343@gmail.com>
2024-10-29 16:26:28 -0300

    pagina cadastro feita e estilizada

--- concertcars/src/app/cadastro/page.tsx
+++ concertcars/src/app/cadastro/page.tsx
@@ -1,7 +1,46 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Cadastro() {
   return (
-    <div>Cadastro</div>
+    <div className='ca-registerContainer'>
+      <form id="form-register" className='ca-registerForm'>
+        <h2>Cadastro</h2>
+        <input
+            type="text"
+            name="txtName"
+            id="idName"
+            className='ca-inputField'
+            placeholder="Insira seu nome"
+            required
+        />
+        <input
+            type="text"
+            name="txtConfirmName"
+            id="idConfirmName"
+            className='ca-inputField'
+            placeholder="Confirme seu nome"
+            required
+        />
+        <input
+            type="password"
+            name="txtPassword"
+            id="idPassword"
+            className='ca-inputField'
+            placeholder="Insira sua senha"
+            required
+        />
+        <input
+            type="password"
+            name="txtConfirmPassword"
+            id="idConfirmPassword"
+            className='ca-inputField'
+            placeholder="Confirme sua senha"
+            required
+        />
+        <button type="submit" className='ca-submitButton'>Cadastrar</button>
+        <Link className='ca-loginLink' href={"/login"}>Já possui uma conta? Login</Link>
+      </form>
+    </div>
   )
 }
