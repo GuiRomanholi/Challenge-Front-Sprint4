b9673f99a26c00ef4bf244c2fd270f4443e2a0dd
GuiRomanholi <guilhermeroma343@gmail.com>
2024-10-31 18:52:39 -0300

    Amem , deu bom

--- concertcars/src/app/api/cliente/[cpf]/route.ts
+++ concertcars/src/app/api/cliente/[cpf]/route.ts
@@ -1,31 +1,43 @@
 import { NextResponse } from "next/server";
 import { TipoCliente } from "@/types";
 
-export async function GET(request: Request, { params }: { params: { cpf: string } }) {
-    try {
-        const { cpf } = params;
-        if (!cpf) {
-            return NextResponse.json({ error: "CPF não fornecido." }, { status: 400 });
-        }
+interface baseParams {
+    cpf: string
+}
 
-        const response = await fetch(`http://localhost:8080/concertcars/clientes/${cpf}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+// Definindo um tipo que encapsula `params`
+interface RouteContext {
+    params: Promise<baseParams>;
+}
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            console.error("Erro na API externa:", errorData);
-            throw new Error(`Erro na API: ${errorData.error || response.statusText}`);
-        }
-
-        const produto: TipoCliente = await response.json();
-
-        return NextResponse.json(produto);
-    } catch (error) {
-        console.error("Erro ao buscar cliente:", error);
-        return NextResponse.json({ error: "Erro ao acessar os dados." }, { status: 500 });
+export async function GET(
+  request: Request,
+  { params }: RouteContext // Definindo `params` dentro de `RouteContext`
+) {
+  try {
+    const cpf = (await params).cpf; // Acessando `cpf` dentro de `params`
+    if (!cpf) {
+      return NextResponse.json({ error: "CPF não fornecido." }, { status: 400 });
     }
+
+    const response = await fetch(`http://localhost:8080/concertcars/clientes/${cpf}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Erro na API externa:", errorData);
+      throw new Error(`Erro na API: ${errorData.error || response.statusText}`);
+    }
+
+    const cliente: TipoCliente = await response.json();
+
+    return NextResponse.json(cliente);
+  } catch (error) {
+    console.error("Erro ao buscar cliente:", error);
+    return NextResponse.json({ error: "Erro ao acessar os dados." }, { status: 500 });
+  }
 }
